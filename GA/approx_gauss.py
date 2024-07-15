4769294ab49214574028b8faf0133e3e11a3ce95
Agathe Beucher <113443809+agathebeucher@users.noreply.github.com>
2024-07-15 16:26:59 +0200

    Update approx_gauss.py

---
+++ GA/approx_gauss.py
@@ -0,0 +1,27 @@
+from scipy.optimize import curve_fit
+
+def approx_gauss(y_data):
+    # Générer une liste de 5000 valeurs allant de 0 à 4999 avec np.linspace
+    x_data = np.linspace(0, 5000, 5000, dtype=int)
+    
+    # Définir la fonction gaussienne non centrée
+    def gaussienne(x, a, mu, sigma):
+        return a * np.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
+    try :
+        # Ajuster la fonction gaussienne aux données
+        popt, pcov = curve_fit(gaussienne, x_data, y_data, p0=[np.max(y_data), np.argmax(y_data), np.std(y_data)])
+        
+        # popt contient les valeurs optimales pour a, mu et sigma
+        a_opt, mu_opt, sigma_opt = popt
+
+        # Comparer les écarts entre la courbe ajustée et les données réelles
+        y_fit = gaussienne(x_data, *popt)
+        residuals = y_data - y_fit
+
+        # Calculer l'erreur quadratique moyenne (MSE)
+        mse = np.mean(residuals**2)
+        
+        return(mse, y_fit)
+    
+    except RuntimeError as e:
+        return None, None
