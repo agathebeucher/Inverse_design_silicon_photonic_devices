6b02e5e0e6cfc4d02e4fbb4d8d24558668b47f52
Agathe Beucher <113443809+agathebeucher@users.noreply.github.com>
2024-07-15 16:29:32 +0200

    Update balayage_k.py

--- GA/balayage_k.py
+++ GA/balayage_k.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal import argrelextrema
 from scipy import constants as ct
+from .approx_gauss import approx_gauss
 import torch
 
 def conversion_k(k_usi, grating_pitch):
@@ -63,25 +64,36 @@ def calculate_func_neff_f(X_params_without_k_normalized, feedforward_model, devi
     # Prédire les spectre en fréquence pour chaque k
     for i in range(len(k_list_normalized)):
         X_params_tensor = torch.tensor(np.array(X_params_list[i]), dtype=torch.float32).to(device)
-        spectrum_k=feedforward_model(X_params_tensor).to(device)       
+        spectrum_k=feedforward_model(X_params_tensor).to(device)
         spectrum_k_list.append(spectrum_k.detach().cpu().numpy())
+        '''
+        mse, y_fit=approx_gauss(spectrum_k.detach().cpu().numpy())   
+        plt.rcParams["figure.figsize"]= 5,5
+        plt.title(f'{n_eff_one(X_params_list[i]*X_data_array_5000_std+X_data_array_5000_mean, spectrum_k.detach().cpu().numpy(), frequencies)[1]}, mse {mse}, k={X_params_list[i][3]*X_data_array_5000_std[3]+X_data_array_5000_mean[3]}')
+        plt.plot(spectrum_k.detach().cpu().numpy())
+        if y_fit is not None:
+            plt.plot(y_fit)
+        plt.show()
+        '''
     # Prédire les fréquences de résonances et les indices eff pour chaque k
     for i in range(len(spectrum_k_list)) :
-        X_params_denormalized=X_params_list[i]*X_data_array_5000_std+X_data_array_5000_mean
-        n_eff_k, f_k=n_eff_one(X_params_denormalized, spectrum_k_list[i],  frequencies)
+        X_params_denormalized=X_params_list[i]*X_data_array_5000_std+X_data_array_5000_mean 
+        n_eff_k, f_k=n_eff_one(X_params_denormalized, spectrum_k_list[i], frequencies)
         n_k_list_temp.append(n_eff_k)
         f_k_list_temp.append(f_k)
     for i in range(len(n_k_list_temp)):
-        if n_k_list_temp[i]!=[]:
-            mse, y_fit = approx_gauss(spectrum_k_list[i])
+        if n_k_list_temp[i] != []:
+            mse, y_fit=approx_gauss(spectrum_k_list[i])   
             if i!=0 and i!= len(n_k_list_temp)-1:
-                if (n_k_list_temp[i+1]!=[] or n_k_list_temp[i-1]!=[]) and n_k_list_temp[i]!=[]:
-                    n_k_list.append(n_k_list_temp[i][0])
-                    f_k_list.append(f_k_list_temp[i][0])
-        else : 
-            if n_k_list_temp[i]!=[]:
-                n_k_list.append(n_k_list_temp[i][0])
-                f_k_list.append(f_k_list_temp[i][0])
+                if (n_k_list_temp[i+1]!=[] or n_k_list_temp[i-1]!=[]) and y_fit is not None:
+                    if mse<0.0001:
+                        n_k_list.append(n_k_list_temp[i][0])
+                        f_k_list.append(f_k_list_temp[i][0])
+            else : 
+                if y_fit is not None :
+                    if mse<0.0001 :
+                        n_k_list.append(n_k_list_temp[i][0])
+                        f_k_list.append(f_k_list_temp[i][0])
     return(n_k_list, f_k_list)
 
 def determine_initial_trend(values, lookahead=5):
