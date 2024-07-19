9e5f6cb05547425d09be2cd908eac5b8e845d3fc
Agathe Beucher <113443809+agathebeucher@users.noreply.github.com>
2024-07-19 15:40:51 +0200

    Update load_data.py

--- EDA/load_data.py
+++ EDA/load_data.py
@@ -12,3 +12,9 @@ def load_data():
     y_data_array_5000=np.load('/home/beucher/Documents/PRE/PRE/data/y_data_array_5000.npy')
 
     return(X_data_array_5000, y_data_array_5000)
+
+def create_datasets(X_data_array_5000_normalized, y_data_array_5000_normalized, test_size):
+    # Divide the dataset into train/test/val datasets
+    X_train_5000_normalized, test_val_X_5000_normalized, y_train_5000_normalized, test_val_Y_5000_normalized = train_test_split(X_data_array_5000_normalized, y_data_array_5000_normalized, test_size=test_size, random_state=42)
+    X_test_5000_normalized, X_val_5000_normalized, y_test_5000_normalized, y_val_5000_normalized= train_test_split(test_val_X_5000_normalized, test_val_Y_5000_normalized, test_size=0.5, random_state=42)
+    return(X_train_5000_normalized, y_train_5000_normalized, X_test_5000_normalized, y_test_5000_normalized, X_val_5000_normalized, y_val_5000_normalized)
